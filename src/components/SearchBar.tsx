61552f839108668a93d3811493b219da0fc277b8
Guilherme Sousa <guilherssousa@gmail.com>
2022-08-28 14:49:00 -0300

    Make SearchBar more beautiful!

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,5 +1,7 @@
 import styles from "styles/components.module.scss";
 
+import searchImage from "/search.svg";
+
 interface Props {
   state: string;
   setState: (state: string) => void;
@@ -8,11 +10,18 @@ interface Props {
 const SearchBar: React.FC<Props> = ({ state, setState }) => {
   return (
     <div className={styles.searchBarComponent}>
+      <img
+        className={styles.searchBarIllustration}
+        src={searchImage}
+        alt="Search"
+      />
       <input
         className={styles.searchBarInput}
         type="text"
         placeholder="Search by name or entry number"
         value={state}
+        autoCorrect="off"
+        spellCheck={false}
         onChange={(e) => setState(e.target.value)}
       />
     </div>
