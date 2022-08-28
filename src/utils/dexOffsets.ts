751ebb271b6fc2a861c145a62a45fade3f4f2860
Guilherme Sousa <guilherssousa@gmail.com>
2022-08-28 15:11:39 -0300

    add offset utilities for generations

---
+++ src/utils/dexOffsets.ts
@@ -0,0 +1,14 @@
+/**
+ * This file contains offsets for each Generation Dex.
+ */
+
+export default {
+  gen1: [1, 151],
+  gen2: [152, 251],
+  gen3: [252, 386],
+  gen4: [387, 493],
+  gen5: [494, 649],
+  gen6: [650, 721],
+  gen7: [722, 809],
+  gen8: [810, 905],
+};
